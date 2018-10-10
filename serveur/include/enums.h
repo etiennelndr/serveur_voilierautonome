bcbb5918d652d2225c2ea4e595f87c365629d398
etiennelndr <elandure@gmail.com>
2018-10-10 14:13:29 +0200

    Create an enumeration for each message type. At the moment we only need 4 message types.

---
+++ serveur/include/enums.h
@@ -0,0 +1,14 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+
+/**
+ * @brief The MessageType enum is used to check/set the type of the message
+ */
+enum MessageType {
+    BOAT_CONTROL,
+    BOAT_DATA_PERSONNAL,
+    BOAT_DATA_OTHERS,
+    WEATHER_STATION,
+};
+
+#endif // ENUMS_H
