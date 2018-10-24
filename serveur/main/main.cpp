31377f3b88b646738a7f7e09f9d1423e41650a65
Etienne Landuré <elandure@gmail.com>
2018-10-24 11:04:45 +0200

    Change main function

--- serveur/main/main.cpp
+++ serveur/main/main.cpp
@@ -6,11 +6,6 @@ using namespace std;
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     MainWindow mw;
-
-    Message msg;
-    cout << msg.getError() << endl;
-    return 0;
-
     mw.show();
     return app.exec();
 }
